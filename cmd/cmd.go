280df97927cf2acaa65e1ad9b8df2d5bd143fedf
Devin Craig <devin_craig@hotmail.com>
2017-08-29 20:02:25 -0400

    Imports and cmd args

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,3 +1,13 @@
 // cmd.go is the command line utility to interface with go-transform.
 package go-transform
 
+import (
+	"os"
+	"image/png"
+	"fmt"
+)
+
+
+func cmd () {
+	userPng := os.Args[2]	
+}
